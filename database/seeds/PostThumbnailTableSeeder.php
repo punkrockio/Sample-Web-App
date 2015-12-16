f68f2d79333d31b90fb87a3b6b7fe37a2053a439
Nick Mitchell <nick@superlogical.org>
2015-12-16 17:37:11 +1300

    Updates

--- database/seeds/PostThumbnailTableSeeder.php
+++ database/seeds/PostThumbnailTableSeeder.php
@@ -1,17 +1,28 @@
 <?php
 
-use App\Post;
+use \App\Post;
 use Illuminate\Database\Seeder;
 
 class PostThumbnailTableSeeder extends Seeder
 {
+	
+
     /**
      * Seed the tags table
      */
     public function run()
     {
-        foreach(Post::all $post){
-        	$post->thumb_url('/uploads/post/'.$post->id.'.jpg');
+    	$max = 30; 
+
+        foreach(Post::all() as $post){
+
+        	if($post->id < $max){
+        		$post->thumb_url = '/uploads/post/'.$post->id.'.jpg';
+        		$post->save();
+        	}
+        	else{
+        		// $post->thumb_url('/uploads/post/'.$post->id.'.jpg');	
+        	}
         }
     }
 }
